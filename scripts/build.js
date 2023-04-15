5ae01f5a0d32a01dbb4048b39f33396135f102b7
gaohongxiang <2460013249@qq.com>
2023-04-15 18:16:07 +0800

    feat: 生产环境rollup打包

---
+++ scripts/build.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+const execa = require("execa")
+debugger
+const targets = fs.readdirSync("packages").filter((f) => {
+  if (!fs.statSync(`packages/${f}`).isDirectory()) {
+    return false
+  }
+  return true
+})
+async function runParallel(source, iteratorFn) {
+  const ret = []
+  for (const item of source) {
+    const p = Promise.resolve().then(() => iteratorFn(item))
+    ret.push(p)
+  }
+  return Promise.all(ret)
+}
+async function build(target) {
+  await execa("rollup", ["-c", "--environment", `TARGET:${target}`], {
+    stdio: "inherit",
+  })
+}
+runParallel(targets, build)
